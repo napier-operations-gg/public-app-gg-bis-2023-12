f215ea955c79ee6a6d61ac858d4ee1e9c3888e2d
Lucius Fox <lucius-fox-gg@protonmail.com>
2024-01-25 05:03:21 +0100

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import antigravity
 
-apikey=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
+SEED=45278374-076b-4bc2-94a3-b5f43eccd327
 
 def main():
     antigravity.fly()
